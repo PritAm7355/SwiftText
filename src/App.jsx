70aede1c8411e5501e32e768758491e7959e719a
Pritam Kale <pritamtk236@gmail.com>
2025-03-04 10:19:21 +0530

    Routes path added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import About from "./components/About"
-import Navbar from "./components/Navbar"
-import TextForm from "./components/TextForm"
-import { useState } from "react"
-
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const[mode, setMode]=useState('light');//whether dark mode is enable or not
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      document.title = "SwiftText - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title = "SwiftText - Light Mode";
     }
-  }
+  };
 
   return (
-    <div>
-      <Navbar title="Textutils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-      <div className="container">
-      <TextForm heading="Enter the text to analyze below" mode={mode}/>
-      {/* <About/> */}
-
+    <Router>
+      <Navbar title="Textutils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* ✅ Replace Switch with Routes */}
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode} />} />
+        </Routes>
       </div>
-     
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
